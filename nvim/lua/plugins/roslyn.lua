cd5c8c529207d4b1ef14031b6441126e9a1ecf5a
sachin_m <sachin@studyclix.com>
2025-12-22 08:39:24 +0000

    fix(roslyn): fixed 2 client attach problem

--- nvim/lua/plugins/roslyn.lua
+++ nvim/lua/plugins/roslyn.lua
@@ -3,10 +3,21 @@ return {
   ft = "cs",
   opts = {
     filewatching = "roslyn",
-    broad_search = true,
+    broad_search = true, -- you can keep this
   },
   config = function(_, opts)
     require("roslyn").setup(opts)
-    vim.lsp.config("roslyn", {})
+
+    local util = require("lspconfig.util")
+
+    vim.lsp.config("roslyn", {
+      -- choose ONE root for the Roslyn client
+      root_dir = function(fname)
+        -- monorepo root: AssessmentAPI
+        return util.root_pattern("Solutions")(fname)
+          or util.root_pattern(".git")(fname)
+          or vim.fn.expand("~/work/AssessmentAPI")
+      end,
+    })
   end,
 }
