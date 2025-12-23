2f8d83eaa758f2965db56a90ae9939fbdb7833a2
sachin_m <sachin@studyclix.com>
2025-12-23 12:26:24 +0000

    feat(fzf config): Added key binding for fzf and nvim yank and paste key config
    feat(fzf config): Added key binding for fzf and nvim yank and paste key config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,40 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- 1. Visual paste without overwriting yank
+map("x", "p", '"_dP', { desc = "Paste without overwriting yank" })
+
+-- 2. Delete / change without yanking
+map("n", "<leader>d", '"_d', { desc = "Delete without yank" })
+map("n", "<leader>D", '"_D', { desc = "Delete line without yank" })
+map("n", "<leader>c", '"_c', { desc = "Change without yank" })
+map("n", "<leader>C", '"_C', { desc = "Change line without yank" })
+
+-- 3. Make x a real delete (not cut)
+map("n", "x", '"_x', { desc = "Delete char without yank" })
+
+-- Move lines up/down (normal + visual)
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Keep cursor centered on half-page jumps and
+map("n", "<C-d>", "<C-d>zz", { desc = "Half-page down, center" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Half-page up, center" })
+-- map("n", "n", "nzzzv", { desc = "Next match, center" })
+-- map("n", "N", "Nzzzv", { desc = "Prev match, center" })
+
+-- Clear  highlight
+map("n", "<Esc>", "<cmd>nohlsearch<CR><Esc>", { desc = "Clear  highlight" })
+
+-- Paste from system clipboard and strip CRs from EOlocal map = vim.keymap.set
+map("n", "<leader>p", function()
+  -- paste from system clipboard
+  vim.cmd('normal! "+p')
+  -- strip CR at end of lines
+  vim.cmd([[%s/\r$//e]])
+end, { desc = "Paste from clipboard (strip CRLF)" })
