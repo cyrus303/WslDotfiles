9551721dff0e0ee37881253cb49d5ef92709e7f9
sachin_m <sachin@studyclix.com>
2026-03-31 09:51:39 +0100

    feat(tmux nvim navigation fix): fixed to key to be ctrl all across

--- nvim/lua/plugins/tmux-navigator.lua
+++ nvim/lua/plugins/tmux-navigator.lua
@@ -1,10 +1,10 @@
 return {
   "christoomey/vim-tmux-navigator",
   keys = {
-    { "<M-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Go to the previous pane" },
-    { "<M-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Go to the left pane" },
-    { "<M-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Go to the down pane" },
-    { "<M-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Go to the up pane" },
-    { "<M-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Go to the right pane" },
+    { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Go to the previous pane" },
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Go to the left pane" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Go to the down pane" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Go to the up pane" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Go to the right pane" },
   },
 }
