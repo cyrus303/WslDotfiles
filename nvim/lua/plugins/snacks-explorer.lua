eec7d252efd06c30d06110b5c24ecffe22894233
sachin_m <sachin@studyclix.com>
2026-04-10 15:02:37 +0100

    feat(snacks git explorer): added a toggle to switch between git and normal explorer
    https://github.com/folke/snacks.nvim/discussions/1683#discussioncomment-15187524

--- nvim/lua/plugins/snacks-explorer.lua
+++ nvim/lua/plugins/snacks-explorer.lua
@@ -1,55 +1,61 @@
+local is_git_item = function(item, git_nodes)
+  return vim.iter(git_nodes):any(function(node)
+    if node.dir_status then
+      return vim.fs.relpath(node.path, item.file) ~= nil
+    end
+    return vim.fs.relpath(item.file, node.path) ~= nil
+  end)
+end
+
 return {
   {
     "folke/snacks.nvim",
     keys = {
       {
         "<leader>E",
-        function()
-          Snacks.picker.pick("explorer", {
-            title = "Git Changes",
-            finder = function(opts, ctx)
-              local Tree = require("snacks.explorer.tree")
-              local nodes = {}
-              Tree:walk(Tree:find(ctx.filter.cwd), function(node)
-                if node.status then
-                  table.insert(nodes, node)
-                end
-              end)
-              ctx.picker.git_nodes = nodes
-              return require("snacks.picker.source.explorer").explorer(opts, ctx)
-            end,
-            transform = function(item, ctx)
-              return vim.iter(ctx.picker.git_nodes):any(function(node)
-                if node.dir_status then
-                  return vim.fs.relpath(node.path, item.file) ~= nil
-                end
-                return vim.fs.relpath(item.file, node.path) ~= nil
-              end)
-            end,
-            layout = { preset = "sidebar" },
-          })
-        end,
-        desc = "Explorer (git changes)",
+        function() Snacks.picker.git_status() end,
+        desc = "Git Status (sidebar)",
       },
     },
     opts = function(_, opts)
-      -- Configure explorer picker source to show hidden files by default
       opts.picker = opts.picker or {}
       opts.picker.sources = opts.picker.sources or {}
       opts.picker.sources.explorer = vim.tbl_deep_extend("force", opts.picker.sources.explorer or {}, {
         hidden = true,
         ignored = true,
+        finder = function(o, ctx)
+          local Tree = require("snacks.explorer.tree")
+          local git_nodes = {}
+          Tree:walk(Tree:find(ctx.picker:cwd()), function(node)
+            if node.status then
+              table.insert(git_nodes, node)
+            end
+          end)
+          ctx.picker.git_nodes = git_nodes
+          return require("snacks.picker.source.explorer").explorer(o, ctx)
+        end,
+        transform = function(item, ctx)
+          if ctx.picker.opts.only_git then
+            return is_git_item(item, ctx.picker.git_nodes)
+          end
+        end,
+        only_git = false,
+        toggles = {
+          only_git = "S",
+        },
+        win = {
+          list = {
+            keys = {
+              ["<CR>"] = { "edit", mode = "n" },
+              ["s"] = { "edit_split", mode = "n" },
+              ["v"] = { "edit_vsplit", mode = "n" },
+              ["S"] = "toggle_only_git",
+            },
+          },
+        },
       })
 
-      -- your window / key mappings (keep these)
       opts.explorer = opts.explorer or {}
-      opts.explorer.win = opts.explorer.win or {}
-      opts.explorer.win.list = opts.explorer.win.list or {}
-      opts.explorer.win.list.keys = vim.tbl_deep_extend("force", opts.explorer.win.list.keys or {}, {
-        ["<CR>"] = { "edit", mode = "n" },
-        ["s"] = { "edit_split", mode = "n" },
-        ["v"] = { "edit_vsplit", mode = "n" },
-      })
     end,
   },
 }
