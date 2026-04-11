60b1b6228f1bb3dbb3dc140f625dc9e025c57404
sachin_m <sachin@studyclix.com>
2026-04-11 21:12:50 +0100

    feat(nvim dap view): setup and plugin updates
    feat(nvim dap view): setup and plugin updates

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -14,6 +14,12 @@ return {
       -- let easy-dotnet supply dap.configurations.cs
       dap.configurations.cs = dap.configurations.cs or {}
 
+      vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpoint" })
+      vim.fn.sign_define("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition" })
+      vim.fn.sign_define("DapBreakpointRejected", { text = "●", texthl = "DapBreakpointRejected" })
+      vim.fn.sign_define("DapStopped", { text = "▶", texthl = "DapStopped", linehl = "DapStoppedLine" })
+      vim.api.nvim_set_hl(0, "DapStoppedLine", { bg = "#2e3a2e" })
+
       local map = vim.keymap.set
       map("n", "<F5>", function()
         dap.continue()
@@ -30,35 +36,56 @@ return {
       map("n", "<leader>db", function()
         dap.toggle_breakpoint()
       end, { desc = "DAP Toggle Breakpoint" })
-      map("n", "<leader>dc", function()
+      map("n", "<leader>dx", function()
         dap.terminate()
+      end, { desc = "DAP Terminate" })
+      map("n", "<leader>dX", function()
         dap.clear_breakpoints()
-      end, { desc = "DAP Stop and Clear Breakpoint" })
-
-      -- Hover variable value while debugging
-      map("n", "<leader>dh", function()
-        require("dap.ui.widgets").hover()
-      end, { desc = "DAP Hover variable" })
+      end, { desc = "DAP Clear Breakpoints" })
+      map("n", "<leader>dB", function()
+        dap.set_breakpoint(vim.fn.input("Condition: "))
+      end, { desc = "DAP Conditional Breakpoint" })
+      map("n", "<leader>dr", function()
+        dap.restart()
+      end, { desc = "DAP Restart" })
+      map("n", "<leader>dR", function()
+        dap.run_to_cursor()
+      end, { desc = "DAP Run to Cursor" })
     end,
   },
 
-  { "nvim-neotest/nvim-nio" },
-
   {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-    config = function()
-      local dap, dapui = require("dap"), require("dapui")
-      dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
+    "igorlfs/nvim-dap-view",
+    version = "1.*",
+    dependencies = { "mfussenegger/nvim-dap" },
+    ---@module 'dap-view'
+    ---@type dapview.Config
+    opts = {
+      auto_toggle = true,
+      windows = {
+        position = "right",
+        size = 0.45,
+        terminal = {
+          position = "below",
+          size = 0.45,
+        },
+      },
+    },
+    config = function(_, opts)
+      local dapview = require("dap-view")
+      dapview.setup(opts)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "dap-view",
+        callback = function(args)
+          vim.keymap.set("n", "<C-l>", "<CR>", { buffer = args.buf, remap = true, desc = "DAP View Expand" })
+        end,
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "dap-view-term", "dap-repl" },
+        callback = function()
+          vim.wo.wrap = true
+        end,
+      })
     end,
   },
 }
