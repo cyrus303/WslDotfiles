0a2f8a86a96034c1c90d24a5dd59cf7179cac86a
sachin_m <sachin@studyclix.com>
2026-04-17 16:52:29 +0100

    chore(fixed todo comments)

--- nvim-minimal/lua/plugins/todo-comments.lua
+++ nvim-minimal/lua/plugins/todo-comments.lua
@@ -3,7 +3,7 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   event = "VeryLazy",
   keys = {
-    { "<leader>st", "<cmd>TodoPicker<cr>", desc = "Todo (Picker)" },
+    { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo (Picker)" },
     { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo" },
     { "[t", function() require("todo-comments").jump_prev() end, desc = "Prev todo" },
   },
