96c39cefc4c51ed8d7d2077d77c44306cc4186c4
sachin_m <sachin@studyclix.com>
2026-04-18 13:15:55 +0100

    feat(fixed lua formating)

--- nvim-minimal/lua/plugins/conform.lua
+++ nvim-minimal/lua/plugins/conform.lua
@@ -1,11 +1,12 @@
 return {
-  "stevearc/conform.nvim",
-  cmd = { "ConformInfo" },
-  opts = {
-    formatters_by_ft = {
-      json = { "biome" },
-      lua = { "stylua" },
-    },
-    format_on_save = false,
-  },
+	"stevearc/conform.nvim",
+	cmd = { "ConformInfo" },
+	event = "BufWritePre",
+	opts = {
+		formatters_by_ft = {
+			json = { "biome" },
+			lua = { "stylua" },
+		},
+		format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+	},
 }
