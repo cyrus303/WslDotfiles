113baadb54580f93d9cf2b05ad73f6faed5c4767
sachin_m <sachin@studyclix.com>
2026-04-21 10:03:57 +0100

    fix(removed gp and gP - yanky): removed conflicting keymaps

--- nvim-minimal/lua/plugins/yanky.lua
+++ nvim-minimal/lua/plugins/yanky.lua
@@ -20,8 +20,6 @@ return {
 		{ "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank" },
 		{ "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put after" },
 		{ "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put before" },
-		{ "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put after selection" },
-		{ "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put before selection" },
 		{ "[y", "<Plug>(YankyCycleForward)", desc = "Cycle forward in yank history" },
 		{ "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle backward in yank history" },
 		{ "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after (linewise)" },
