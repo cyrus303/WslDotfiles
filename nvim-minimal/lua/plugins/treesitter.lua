4862798ac470c1b8cb7c53f3332603dfa71547a8
Sachin Mahesh <sachinsunny303@gmail.com>
2026-04-27 20:25:47 +0100

    feat(nvim treesitter): Fixed nvim tree sitter issues for nvim v 0.12
    feat(nvim treesitter): Fixed nvim tree sitter issues for nvim v 0.12
    
    feat(nvim treesitter): Fixed nvim tree sitter issues for nvim v 0.12
    
    feat(nvim treesitter): Fixed nvim tree sitter issues for nvim v 0.12

--- nvim-minimal/lua/plugins/treesitter.lua
+++ nvim-minimal/lua/plugins/treesitter.lua
@@ -1,74 +1,69 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    dependencies = {
-      { "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
-    },
-    opts = {
-      highlight = {
-        enable = true,
-        -- Let Roslyn LSP handle C# highlighting; parser is still used for text objects/folds
-        disable = { "c_sharp" },
-      },
-      indent = { enable = true },
-      ensure_installed = {
-        "bash",
-        "c_sharp",
-        "diff",
-        "html",
-        "http",
-        "javascript",
-        "json",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "query",
-        "regex",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["al"] = "@loop.outer",
-            ["il"] = "@loop.inner",
-            ["aC"] = "@call.outer",
-            ["iC"] = "@call.inner",
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
+		dependencies = {
+			{ "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
+		},
+		config = function()
+			require("nvim-treesitter").install({
+				"bash",
+				"c_sharp",
+				"diff",
+				"json",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+				"xml",
+				"yaml",
+			})
+
+			-- Enable treesitter highlighting for all filetypes.
+			-- Skip cs: Roslyn LSP handles C# highlighting via semantic tokens.
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(ev)
+					if ev.match ~= "cs" then
+						pcall(vim.treesitter.start)
+					end
+				end,
+			})
+
+			-- Treesitter-based indentation (experimental but functional for most langs)
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+
+			-- Textobjects
+			require("nvim-treesitter-textobjects").setup({
+				select = { lookahead = true },
+			})
+
+			local sel = require("nvim-treesitter-textobjects.select").select_textobject
+			local maps = {
+				af = "@function.outer",
+				["if"] = "@function.inner",
+				ac = "@class.outer",
+				ic = "@class.inner",
+				aa = "@parameter.outer",
+				ia = "@parameter.inner",
+				al = "@loop.outer",
+				il = "@loop.inner",
+				aC = "@call.outer",
+				iC = "@call.inner",
+			}
+			for key, obj in pairs(maps) do
+				vim.keymap.set({ "x", "o" }, key, function()
+					sel(obj, "textobjects")
+				end, { desc = "Textobj " .. obj })
+			end
+		end,
+	},
 }
