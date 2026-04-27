465afb64a54a495795ed7fffc08ab3d00548f672
Sachin Mahesh <sachinsunny303@gmail.com>
2026-04-27 19:51:10 +0100

    chore(added telescop): added keymaps for telescope

---
+++ nvim-minimal/lua/plugins/telescope.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  optional = true,
+  opts = function()
+    local actions = require("telescope.actions")
+    return {
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-l>"] = actions.select_default,
+          },
+          n = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-l>"] = actions.select_default,
+          },
+        },
+      },
+    }
+  end,
+}
