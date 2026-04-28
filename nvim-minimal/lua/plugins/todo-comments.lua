e72c01188859d9eb0724e1c066b36ec60d0aa6f3
Sachin Mahesh <sachinsunny303@gmail.com>
2026-04-28 12:19:35 +0100

    feat(highlight groups for keywords)
    feat(highlight groups for keywords)

--- nvim-minimal/lua/plugins/todo-comments.lua
+++ nvim-minimal/lua/plugins/todo-comments.lua
@@ -7,5 +7,17 @@ return {
     { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo" },
     { "[t", function() require("todo-comments").jump_prev() end, desc = "Prev todo" },
   },
-  opts = {},
+  opts = {
+    highlight = {
+      keyword = "wide_bg",
+    },
+    colors = {
+      error   = { "#FF6E6E" },
+      warning = { "#FFE66D" },
+      info    = { "#4DFCFF" },
+      hint    = { "#69FF94" },
+      default = { "#BD93F9" },
+      test    = { "#FF79C6" },
+    },
+  },
 }
