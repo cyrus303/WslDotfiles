40a0f11430409c4d009e0bd87daa9e914e850a1f
Sachin Mahesh <sachinsunny303@gmail.com>
2026-04-28 17:06:34 +0100

    feat(roslyn race condition): Error 30099 nvim polls dignostics before roslyn is ready

--- nvim-minimal/lua/plugins/roslyn.lua
+++ nvim-minimal/lua/plugins/roslyn.lua
@@ -22,5 +22,15 @@ return {
   opts = roslyn_opts,
   config = function(_, opts)
     require("roslyn").setup(opts)
+    -- Roslyn returns -30099 when a diagnostic request arrives before it has
+    -- finished initialising the document's language. Swallow it silently.
+    local orig = vim.lsp.handlers["textDocument/diagnostic"]
+    vim.lsp.handlers["textDocument/diagnostic"] = function(err, result, ctx, config)
+      if err and err.code == -30099 then
+        local client = vim.lsp.get_client_by_id(ctx.client_id)
+        if client and client.name == "roslyn" then return end
+      end
+      return orig(err, result, ctx, config)
+    end
   end,
 }
