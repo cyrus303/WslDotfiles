2db14dcd42fd08d0c61b3af4de2ee759e6bb866c
Sachin Mahesh <sachinsunny303@gmail.com>
2026-04-30 22:28:34 +0100

    feat(kill session keybind): prefix k

--- tmux/popup.sh
+++ tmux/popup.sh
@@ -4,6 +4,7 @@ DIR="${1:-$HOME}"
 
 tmux has-session -t "$SESSION" 2>/dev/null || tmux new-session -d -s "$SESSION" -c "$DIR"
 
+tmux set-option -t "$SESSION" detach-on-destroy on
 tmux set-option -t "$SESSION" status on
 tmux set-option -t "$SESSION" status-position top
 tmux set-option -t "$SESSION" status-style "bg=#0e0e16"
